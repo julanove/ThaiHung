e219302f592b116f8f97d2ca96f2eabe9c2f0eb5
julanove <yuna.love.26@gmail.com>
2020-06-22 16:27:01 +0900

    client almost

--- config/config.js
+++ config/config.js
@@ -25,12 +25,14 @@
   },
 production: {
    
-    url: 'http://my.site.com',
+    url: "https://thaihung.herokuapp.com/",
 
     database: {
-        host: '127.0.0.1',
-        port: '27017',
-        db:     'site'
+        host: "18.178.72.199",
+        user: 'yukina',
+        password: 'yukina@123',
+        database: 'yukina',
+        multipleStatements: true
     },
 }
 };
