bddb1fba43a258bb3344ee26e0d24951846cee2b
julanove <yuna.love.26@gmail.com>
2020-07-30 14:47:07 +0900

    captcha 3

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,43 +1,35 @@
 $(document).on("click", ".toiawase", function (e) {
 
-    e.preventDefault();
-    grecaptcha.ready(function () {
-        grecaptcha.execute('reCAPTCHA_6LfrYawZAAAAAA3LxPNto_hxdTHYkAaGWv-TpKU1', { action: 'submit' }).then(function (token) {
+    var company = $('#company').val();
+    var name = $('#name').val();
+    var country = $('#country').val();
+    var content = $('#content').val();
+    var checkbox = $('#checkbox').val();
 
 
-            var company = $('#company').val();
-            var name = $('#name').val();
-            var country = $('#country').val();
-            var content = $('#content').val();
+    console.log("----2");
+    console.log(checkbox);
 
-            console.log("----2");
+    var websiteURL = $('#websiteURL').val() + '/contact';
+    $('#gif').css('visibility', 'visible');
 
-            var websiteURL = $('#websiteURL').val() + '/contact';
-            $('#gif').css('visibility', 'visible');
-
-            $.ajax({
-                url: '/contact',
-                type: "POST",
-                data: JSON.stringify({ company: company, name: name, country: country, content: content }),
-                dataType: "json",
-                contentType: "application/json",
-                success: function (result) {
-                    alert("OK");
-                    $('#company').val("");
-                    $('#name').val("");
-                    $('#country').val("");
-                    $('#content').val("");
-                    $('#gif').css('visibility', 'hidden');
-                },
-                error: function (result) {
-                    alert("error");
-                }
-            })
-
-        });
+    $.ajax({
+        url: '/contact',
+        type: "POST",
+        data: JSON.stringify({ company: company, name: name, country: country, content: content }),
+        dataType: "json",
+        contentType: "application/json",
+        success: function (result) {
+            alert("OK");
+            $('#company').val("");
+            $('#name').val("");
+            $('#country').val("");
+            $('#content').val("");
+            $('#gif').css('visibility', 'hidden');
+        },
+        error: function (result) {
+            alert("error");
+        }
     });
-
-    
-
 });
 
