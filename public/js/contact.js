69542f96b724c06df1e14a9719b403e9e58350e0
julanove <yuna.love.26@gmail.com>
2020-07-30 14:24:51 +0900

    add captcha

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,40 +1,43 @@
 $(document).on("click", ".toiawase", function (e) {
 
-    //e.preventDefault();
-    //grecaptcha.ready(function () {
-    //    grecaptcha.execute('reCAPTCHA_site_key', { action: 'submit' }).then(function (token) {
-    //        console.log('OK');
-    //    });
-    //});
+    e.preventDefault();
+    grecaptcha.ready(function () {
+        grecaptcha.execute('6LfrYawZAAAAAA3LxPNto_hxdTHYkAaGWv-TpKU1', { action: 'submit' }).then(function (token) {
 
-    var company = $('#company').val();
-    var name = $('#name').val();
-    var country = $('#country').val();
-    var content = $('#content').val();
 
-    console.log("----2");
+            var company = $('#company').val();
+            var name = $('#name').val();
+            var country = $('#country').val();
+            var content = $('#content').val();
 
-    var websiteURL = $('#websiteURL').val() + '/contact';
-    $('#gif').css('visibility', 'visible');
+            console.log("----2");
 
-    $.ajax({
-        url: '/contact',
-        type: "POST",
-        data: JSON.stringify({company: company, name: name, country: country, content: content }),
-        dataType: "json",
-        contentType: "application/json",
-        success: function (result) {
-            alert("OK");
-            $('#company').val("");
-            $('#name').val("");
-            $('#country').val("");
-            $('#content').val("");
-            $('#gif').css('visibility', 'hidden');
-        },
-            error: function (result) {
-            alert("error");
-        }
-    })
+            var websiteURL = $('#websiteURL').val() + '/contact';
+            $('#gif').css('visibility', 'visible');
+
+            $.ajax({
+                url: '/contact',
+                type: "POST",
+                data: JSON.stringify({ company: company, name: name, country: country, content: content }),
+                dataType: "json",
+                contentType: "application/json",
+                success: function (result) {
+                    alert("OK");
+                    $('#company').val("");
+                    $('#name').val("");
+                    $('#country').val("");
+                    $('#content').val("");
+                    $('#gif').css('visibility', 'hidden');
+                },
+                error: function (result) {
+                    alert("error");
+                }
+            })
+
+        });
+    });
+
+    
 
 });
 
